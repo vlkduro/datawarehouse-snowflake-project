ec9dd052b55a0429d7fca080085d387beecca3f5
StutenEXE <alexandre.bidaux78@gmail.com>
2025-05-27 08:51:00 +0200

    Script initialisation des BDD

---
+++ db/init.sql
@@ -0,0 +1,35 @@
+-- Postgres n'accepte pas la syntaxe CREATE DATABASE IF NOT EXISTS
+-- On utilise un bloc DO pour vérifier l'existence de la base de données avant de la créer
+
+-- Creation de STG si la base n'existe pas
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'STG') THEN
+        CREATE DATABASE STG;
+    END IF;
+END
+$$;
+
+-- Creation de WRK si la base n'existe pasDO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'WRK') THEN
+        CREATE DATABASE WRK;
+    END IF;
+END
+$$;
+
+-- Creation de SOC si la base n'existe pasDO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'SOC') THEN
+        CREATE DATABASE SOC;
+    END IF;
+END
+$$;
+
+-- Creation de TCH si la base n'existe pasDO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'TCH') THEN
+        CREATE DATABASE TCH;
+    END IF;
+END
+$$;
